27b6b887311744017cddb2b595f15de6865271d3
Paul Gaudeaux <phvcgdx@gmail.com>
2024-05-27 10:14:47 +0200

    setup(web-app): web app creation

---
+++ web-app/src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import styles from './Home.module.scss';
+
+export const Home: FC = () => {
+  return (
+    <div className={styles.homeContainer}>This is my app</div>
+  )
+}
---
+++ web-app/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'ES2022',
+    emptyOutDir: true,
+    outDir: 'build',
+    rollupOptions: {
+      // Flatten because I had issues with nested folders in nginx
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+})
