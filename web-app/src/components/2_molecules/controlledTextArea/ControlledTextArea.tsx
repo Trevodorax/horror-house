f3a6efb02d8c2fdd83885dd7b1e9323f7dbe8341
Paul Gaudeaux <phvcgdx@gmail.com>
2024-07-12 16:20:24 +0200

    dev(lint): improve biome config

--- web-app/src/components/2_molecules/controlledTextArea/ControlledTextArea.tsx
+++ web-app/src/components/2_molecules/controlledTextArea/ControlledTextArea.tsx
@@ -1,18 +1,17 @@
 import { InputInfoWrapper } from "@/components/1_atoms/inputInfoWrapper/InputInfoWrapper";
-import { type FC, forwardRef } from "react";
+import { forwardRef } from "react";
 
 interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
 	label: string;
 	errorMessage?: string;
 }
 
-export const ControlledTextArea = forwardRef<
-	HTMLTextAreaElement,
-	Props
->(({ label, errorMessage, ...textAreaProps }, ref) => {
-	return (
-		<InputInfoWrapper label="Message" errorMessage={errorMessage}>
-			<textarea ref={ref} {...textAreaProps} />
-		</InputInfoWrapper>
-	);
-});
+export const ControlledTextArea = forwardRef<HTMLTextAreaElement, Props>(
+	({ label, errorMessage, ...textAreaProps }, ref) => {
+		return (
+			<InputInfoWrapper label="Message" errorMessage={errorMessage}>
+				<textarea ref={ref} {...textAreaProps} />
+			</InputInfoWrapper>
+		);
+	},
+);
