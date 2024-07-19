b32a9d7bdea16234792726ad64e1b975c38c0449
Paul Gaudeaux <phvcgdx@gmail.com>
2024-07-19 10:29:42 +0200

    feat(theme): create theme context and apply it

---
+++ web-app/src/components/3_organisms/themeContext/ThemeContext.test.tsx
@@ -0,0 +1,70 @@
+import { act, render, screen } from "@/tests/react-testing-library";
+import { describe, expect, it } from "vitest";
+import { ThemeProvider, useTheme } from "./ThemeContext";
+
+// Helper component to test the context
+const TestComponent = () => {
+	const { toggleTheme, isDark } = useTheme();
+	return (
+		<div>
+			<p data-testid="theme-state">{isDark ? "dark" : "light"}</p>
+			<button type="button" onClick={toggleTheme} data-testid="toggle-btn">
+				Toggle
+			</button>
+		</div>
+	);
+};
+
+describe("ThemeProvider", () => {
+	it("should initialize with the theme light", () => {
+		render(
+			<ThemeProvider>
+				<TestComponent />
+			</ThemeProvider>,
+		);
+		expect(screen.getByTestId("theme-state").textContent).toBe("light");
+	});
+
+	it("should toggle the theme state", () => {
+		render(
+			<ThemeProvider>
+				<TestComponent />
+			</ThemeProvider>,
+		);
+
+		const toggleBtn = screen.getByTestId("toggle-btn");
+
+		// Initially light
+		expect(screen.getByTestId("theme-state").textContent).toBe("light");
+
+		// toggle theme
+		act(() => {
+			toggleBtn.click();
+		});
+		expect(screen.getByTestId("theme-state").textContent).toBe("dark");
+
+		// toggle theme
+		act(() => {
+			toggleBtn.click();
+		});
+		expect(screen.getByTestId("theme-state").textContent).toBe("light");
+	});
+
+	it("should throw an error if useTheme is used outside of ThemeProvider", () => {
+		const ErrorComponent = () => {
+			try {
+				useTheme();
+				return <div>No error</div>;
+			} catch (e: unknown) {
+				if (e instanceof Error) {
+					return <div>{e.message}</div>;
+				}
+			}
+		};
+
+		const { container } = render(<ErrorComponent />);
+		expect(container.textContent).toBe(
+			"useTheme must be used within a ThemeProvider",
+		);
+	});
+});
--- web-app/src/components/4_templates/globalLayout/GlobalLayout.tsx
+++ web-app/src/components/4_templates/globalLayout/GlobalLayout.tsx
@@ -7,19 +7,22 @@ import { GlobalHeader } from "../../3_organisms/globalHeader/GlobalHeader";
 import { GlobalFooter } from "@/components/3_organisms/globalFooter/GlobalFooter";
 import { SidebarProvider } from "@/components/3_organisms/sidebarContext/SidebarContext";
 import styles from "./GlobalLayout.module.scss";
+import { ThemeProvider } from "@/components/3_organisms/themeContext/ThemeContext";
 
 export const GlobalLayout: FC = () => {
 	return (
-		<SidebarProvider>
-			<div className={styles.globalLayoutContainer}>
-				<GlobalHeader />
-				<main className={styles.mainContainer}>
-					<Surface className={styles.globalLayoutOutletContainer}>
-						<Outlet />
-					</Surface>
-				</main>
-				<GlobalFooter />
-			</div>
-		</SidebarProvider>
+		<ThemeProvider>
+			<SidebarProvider>
+				<div className={styles.globalLayoutContainer}>
+					<GlobalHeader />
+					<main className={styles.mainContainer}>
+						<Surface className={styles.globalLayoutOutletContainer}>
+							<Outlet />
+						</Surface>
+					</main>
+					<GlobalFooter />
+				</div>
+			</SidebarProvider>
+		</ThemeProvider>
 	);
 };
