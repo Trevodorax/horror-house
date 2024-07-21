14a4217eabaf452585b8256f9068abadf44cc57a
Paul Gaudeaux <phvcgdx@gmail.com>
2024-07-21 18:54:01 +0200

    feat(sessions): list and delete sessions

--- web-app/src/components/3_organisms/actionsHeader/ActionsHeader.tsx
+++ web-app/src/components/3_organisms/actionsHeader/ActionsHeader.tsx
@@ -6,19 +6,26 @@ import { Button } from "../../2_molecules/button/Button";
 
 import { Text } from "../../1_atoms/text/Text";
 import styles from "./ActionsHeader.module.scss";
+import { TrashIcon } from "@/components/1_atoms/icons/TrashIcon";
 
 interface Props {
 	title?: string;
 	close?: () => void;
+	remove?: () => void;
 }
 
-export const ActionsHeader: FC<Props> = ({ close, title = "" }) => {
+export const ActionsHeader: FC<Props> = ({ remove, close, title = "" }) => {
 	return (
-		<Surface className={styles.headerContainer}>
+		<Surface className={styles.headerContainer} background="transparent">
 			<Text type="sectionHeading">{title}</Text>
 			{close && (
 				<Button aria-label="close" onClick={close}>
 					<CloseIcon className={styles.actionIcon} />
+				</Button>
+			)}
+			{remove && (
+				<Button variant="error" aria-label="remove" onClick={remove}>
+					<TrashIcon className={styles.actionIcon} />
 				</Button>
 			)}
 		</Surface>
